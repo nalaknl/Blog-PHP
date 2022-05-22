a5f2fc106e8f026cf545aa72b27a87927ddef8c3
nalaknl <sebastien.leriche@hotmail.fr>
2022-05-22 09:18:16 +0200

    cool

--- add-article.php
+++ add-article.php
@@ -8,13 +8,13 @@
 <head>
     <?php require_once "includes/head.php" ?>
     <title>APP</title>
+    <link rel="stylesheet" href="./public/css/add-article.css">
 </head>
 
 
 <body>
     <div class="container">
         <?php require_once "includes/header.php" ?>
-        <link rel="stylesheet" href="./public/css/index.css">
 
 
 
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,9 @@
 <header>
     <div class="logo">Blog app</div>
 </header>
+<a class="logo" href="/">Blog app</a>
+<ul class="header-menu">
+    <li class="<?= $_SERVER["REQUEST_URI"] === "/add-article.php" ? "active" : "" ?>">
+        <a href="/add-article.php">Ecrire un article</a>
+    </li>
+</ul>
